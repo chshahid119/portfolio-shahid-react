824d8827d2ce25b8b6bd81f62734cdeaf756bc1b
zubairch1199 <133282511+chshahid119@users.noreply.github.com>
2024-03-06 11:47:01 +0500

    many other cards added for projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,23 +8,57 @@ const projectData = [
     name: "amazon",
     description: "react js React hooks",
   },
+  {
+    name: "amazon",
+    description: "react js React hooks",
+  },
+  {
+    name: "amazon",
+    description: "react js React hooks",
+  },
+  {
+    name: "amazon",
+    description: "react js React hooks",
+  },
+  {
+    name: "amazon",
+    description: "react js React hooks",
+  },
+  {
+    name: "amazon",
+    description: "react js React hooks",
+  },
+  {
+    name: "amazon",
+    description: "react js React hooks",
+  },
 ];
 export default function Projects() {
   return (
     <div className="projects">
+      {projectData.map((data) => (
+        <Card data={data} />
+      ))}
+    </div>
+  );
+}
+
+function Card({ card }) {
+  console.log(card);
+  return (
+    <>
       <div className="projects__cards">
         <a href="#">
           <img projects__cards-img src={Image} alt="image description" />
           <h2>Discord Clone</h2>
-          <p>
-            this project contains all the functionalities of discord with its
-            all functionalities etc are here.
-          </p>
+
           <div className="projects__cards-skillUsed">
-            <p>HTML</p> <p>HTML</p> <p>HTML</p>
+            <p>HTML</p> <p>CSS</p> <p>JavaScript</p> <p>JavaScript</p>{" "}
+            <p>JavaScript</p>
+            <p>JavaScript</p>
           </div>
         </a>
       </div>
-    </div>
+    </>
   );
 }
