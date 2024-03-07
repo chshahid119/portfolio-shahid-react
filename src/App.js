432dc2183015da63ea1924073565121056ab7965
zubairch1199 <133282511+chshahid119@users.noreply.github.com>
2024-03-07 18:09:54 +0500

    All app layout added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import Shahid from "./img/shahidOfficial.PNG";
+
 function App() {
   return (
     <div className="App">
-      <div className="shahid">
-        <p className="btn">Go here</p>
+      <div className="App__header">
+        <img src={Shahid} alt="company logo" />
       </div>
+      <div className="App__hero">Hero Section</div>
+      <div className="App__about">About</div>
+      <div className="App__help">How do i help?</div>
+      <div className="App__experience">My work experience.</div>
+      <div className="App__latestWork">My latest works (projects)</div>
+      <div className="App__skills">Skills Set</div>
+      <div className="App__collaboration">Team Work (FYP)</div>
+      <div className="App__testemonials">Testemonials</div>
+      <div className="App__form">Contact or Email Form </div>
+      <div className="App__footer">Footer section</div>
     </div>
   );
 }
