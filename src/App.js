966fa0ccdaeffb848b32c077f1dcf0b1a42e6eb9
zubairch1199 <133282511+chshahid119@users.noreply.github.com>
2024-03-07 15:10:14 +0500

    add color scheme with tests

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <div className="shahid">
+        <p className="btn">Go here</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
