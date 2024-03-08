6d3155187399b38976a75d1c3c89838b525467ae
zubairch1199 <133282511+chshahid119@users.noreply.github.com>
2024-03-09 03:03:19 +0500

    add svg in hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,3 +1,9 @@
+import HeroBgDots from "./sub-components/HeroBgDots";
+
 export default function Hero() {
-  return <div className="hero"></div>;
+  return (
+    <div className="hero">
+      <HeroBgDots className="hero_bg-dots" />
+    </div>
+  );
 }
