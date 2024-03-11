92c4c91240204a3242e0913f4e26d308f24f3bdc
zubairch1199 <133282511+chshahid119@users.noreply.github.com>
2024-03-11 06:18:37 +0500

    add experience some section

--- src/components/WorkExp.js
+++ src/components/WorkExp.js
@@ -5,9 +5,33 @@ export default function WorkExp() {
     <div className="workExp">
       <Headings>My Working Experience</Headings>
       <div className="workExp__container">
-        <div className="roles">sdf</div>
-        <div className=""></div>
+        <ExperienceList />
+        <ExperienceList />
       </div>
     </div>
+  );
+}
+
+function ExperienceList() {
+  return (
+    <>
+      <div className="workExp__container--frame">
+        <div>
+          <h3>Safeer-e-Makkah Travels</h3>
+          <p>Sep 2019 - Feb 2020</p>
+        </div>
+      </div>
+      <div className="workExp__container--description">
+        <div className="workExp__container--description-li">
+          <div>&nbsp;</div>
+          <h3>Ticketing Manager</h3>
+          <p>
+            Work as ticketing manager in Safeer-e-Makkah Travel and Tours
+            Private Limited. Control All tickets management across various
+            platforms.
+          </p>
+        </div>
+      </div>
+    </>
   );
 }
